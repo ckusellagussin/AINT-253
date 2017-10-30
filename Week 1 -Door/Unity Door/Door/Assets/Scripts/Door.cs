47a743cff9c2a91a04d0d7fe4ac8e793a2d79abd
Casey Kusella-Gussin <casey.kusella-gussin@students.plymouth.ac.uk>
2017-10-30 00:15:37 +0000

    Mark's Room and Door
    Thats basically it

---
+++ Week 1 -Door/Unity Door/Door/Assets/Scripts/Door.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+
+    private int m_LI;
+
+    public void PlayDoorAnim()
+    {
+
+        
+            if (m_LI == 0)
+            {
+                GetComponent<Animation>().Play("Pivot");
+                m_LI = 1;
+
+
+
+
+            }
+
+            else
+            {
+                GetComponent<Animation>().Play("Pivot_Close");
+                m_LI = 0;
+
+            }
+
+        }
+
+    }
+
+
---
+++ Week 1 -Door/Unity Door/Door/Assets/Scripts/InputHandlerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandlerScript : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetMouseButton(0))
+        {
+
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit rayCastHit;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out rayCastHit, Mathf.Infinity))
+            {
+
+                Debug.Log("Mouse Click Worked Bro");
+                Door door = rayCastHit.transform.GetComponent<Door>();
+
+                if (door)
+                {
+                    door.PlayDoorAnim();
+
+                }
+            }
+
+
+        }
+	
+	}
+}
