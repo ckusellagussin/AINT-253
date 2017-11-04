d3e454f7932ee567a9b477535513d9c3e3cf28c0
Casey Kusella-Gussin <casey.kusella-gussin@students.plymouth.ac.uk>
2017-11-04 19:24:29 +0000

    Door update
    Door has had a massive update

---
+++ Week 1 -Door/Unity Door/Door/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    private Animator _animator;
+
+    public GameObject OpenPanel = null;
+
+    private bool _isInsideTrigger = false;
+
+	// Use this for initialization
+	void Start () {
+        _animator = transform.Find("Right_SideReal").GetComponent<Animator>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            OpenPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            _animator.SetBool("open", false);
+            OpenPanel.SetActive(false);
+        }
+    }
+
+    private bool IsOpenPanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if(IsOpenPanelActive && _isInsideTrigger)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                OpenPanel.SetActive(false);
+                _animator.SetBool("open", true);  
+            }
+        }
+	}
+}
---
+++ Week 1 -Door/Unity Door/Door/Assets/Scripts/Door_2.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door_2 : MonoBehaviour {
+
+
+    private Animator _animation;
+    public GameObject OpenPanel = null;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        _animation = GetComponent<Animator>();
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            OpenPanel.SetActive(true);
+
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+        {
+        if (other.tag == "Player")
+        {
+            _animation.SetBool("open", false);
+            OpenPanel.SetActive(false);
+
+        }
+
+
+        }
+
+    private bool IsOpenPanePanelActive
+    {
+        get
+        {
+            return OpenPanel.activeInHierarchy;
+
+        }
+
+    }
+
+
+    
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
