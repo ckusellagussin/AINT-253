d5114211aee5c0f777253edef2dc2ad121ebfa7e
Casey Kusella-Gussin <casey.kusella-gussin@students.plymouth.ac.uk>
2018-01-15 15:28:06 +0000

    Fini
    Donezo

---
+++ Street Scene - Down The Game Lane/Street Scene - Down Game Lane/Assets/Assets made by CKG/Scripts/PlaySound.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class PlaySound : MonoBehaviour
+{
+
+    public AudioSource SoundSouce;
+    public AudioClip Sound;
+
+    private bool hasPlayedAudio;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && hasPlayedAudio == false)
+            SoundSouce.PlayOneShot(Sound);
+            hasPlayedAudio = false;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
